01efb4790824d579078536beb7a837f247562f76
Konstantin Shilkov <konst.shilkov@gmail.com>
2025-09-21 21:26:56 +0200

    feat(locales): update localization files for improved game messaging and UI; fix typos and enhance modal prompts

--- src/lib/locales/locale.ts
+++ src/lib/locales/locale.ts
@@ -4,14 +4,4 @@ import fr from "./fr.json";
 
 export type TranslationSchema = typeof en;
 export type Locale = "en" | "ru" | "fr";
-// export type Path<T, Prev extends string = ""> = {
-//   [K in keyof T & string]: T[K] extends string
-//     ? `${Prev}${K}` // ex "modal.reset.title"
-//     : T[K] extends object
-//     ?
-//         | `${Prev}${K}` // ex "modal.reset"
-//         | Path<T[K], `${Prev}${K}.`> // ex "modal.reset.title"
-//     : never;
-// }[keyof T & string];
-
 export const translations: Record<Locale, TranslationSchema> = { en, ru, fr };
